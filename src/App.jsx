f563ff91f8f751041a0674b380975e2deae6afae
Shivam Yadav <shyadav@microsoft.com>
2020-08-15 16:53:22 +0530

    added redux store

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import './App.css';
 import TodoList from './components/todoList';
 import AddTodo from './components/addTodo';
 import VisibilityFilter from './components/visibilityFilters';
+import {Provider} from 'react-redux'
+import store from './redux/store';
+import './App.css';
 
 function App() {
   const filters = ['all', 'completed', 'incomplete']
   return (
-    <div>
-      <h1>TODO List</h1>
-      <AddTodo></AddTodo>
-      <TodoList todos={[{content: 'Task 1'}, {content: 'Task 2'}]}/>
-      <VisibilityFilter filters={filters}/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <h1>TODO List</h1>
+        <AddTodo></AddTodo>
+        <TodoList todos={[{content: 'Task 1'}, {content: 'Task 2'}]}/>
+        <VisibilityFilter filters={filters}/>
+      </div>
+    </Provider>
   );
 }
 
