80ba750f0af49a4cd468e4c8ad05afd4fdc43336
Shivam Yadav <shyadav@microsoft.com>
2020-08-22 23:03:26 +0530

    removed enchancers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,4 @@
 import { createStore, combineReducers } from 'redux'
 import { todos, visibilityFilter } from './reducers'
 
-export default createStore(combineReducers({ todos, visibilityFilter }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+export default createStore(combineReducers({ todos, visibilityFilter }))
